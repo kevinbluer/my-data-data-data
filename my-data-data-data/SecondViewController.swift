6632ee361645671494d0c65cf7fa893d6e72f3b6
Kevin Bluer <kevin@bluer.com>
2015-04-25 22:25:04 +0800

    Initial Commit

---
+++ my-data-data-data/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  my-data-data-data
+//
+//  Created by Kevin Bluer on 4/25/15.
+//  Copyright (c) 2015 Bluer Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
